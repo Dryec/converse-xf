faf63d0fffd48963ba7b326ca5ad24c43266ed24
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-11-29 18:43:14 +0100

    add GroupChatViewCell and minor ui changes

--- src/Converse/ViewModels/ChatsOverviewPageViewModel.cs
+++ src/Converse/ViewModels/ChatsOverviewPageViewModel.cs
@@ -22,40 +22,51 @@ namespace Converse.ViewModels
 
             ChatEntries = new ObservableCollection<ChatEntry>();
 
-            ChatEntries.Add(new ChatEntry
+            for (var i = 0; i < 1; i++)
             {
-                ChatPartner = new User { Name = "Pascal", ImageUri = new Uri("https://www.tron-society.com/img/team/pascal.jpg") },
-                Type = ChatType.Normal,
-                LastMessage = new ChatMessage { Message = "Hey man! How are you today?" }, UnreadMessagesCount = 6
-            });
-            ChatEntries.Add(new ChatEntry
-            {
-                ChatPartner = new User { Name = "Dave", ImageUri = new Uri("https://www.tron-society.com/img/team/dave.jpg") },
-                Type = ChatType.Normal,
-                LastMessage = new ChatMessage { Message = "See you on monday!" },
-                UnreadMessagesCount = 0
-            });
-            ChatEntries.Add(new ChatEntry
-            {
-                ChatPartner = new User { Name = "Luca", ImageUri = new Uri("https://www.tron-society.com/img/team/luca.jpg") },
-                Type = ChatType.Normal,
-                LastMessage = new ChatMessage { Message = "Bye Bye" },
-                UnreadMessagesCount = 0
-            });
-            ChatEntries.Add(new ChatEntry
-            {
-                ChatPartner = new User { Name = "Marius", ImageUri = new Uri("https://www.tron-society.com/img/team/marius.jpg") },
-                Type = ChatType.Normal,
-                LastMessage = new ChatMessage { Message = "Lol what the hell are you doin?" },
-                UnreadMessagesCount = 43
-            });
-            ChatEntries.Add(new ChatEntry
-            {
-                ChatPartner = new User { Name = "Simon", ImageUri = new Uri("https://www.tron-society.com/img/team/simon.jpg") },
-                Type = ChatType.Normal,
-                LastMessage = new ChatMessage { Message = "When do you have time?" },
-                UnreadMessagesCount = 2
-            });
+                ChatEntries.Add(new ChatEntry
+                {
+                    ChatPartner = new UserInfo { Name = "Pascal", ImageUri = new Uri("https://www.tron-society.com/img/team/pascal.jpg") },
+                    Type = ChatType.Normal,
+                    LastMessage = new ChatMessage { Message = "Hey man! How are you today?", Timestamp = DateTime.Now.AddMinutes(-4) },
+                    UnreadMessagesCount = 6
+                });
+                ChatEntries.Add(new ChatEntry
+                {
+                    ChatPartner = new UserInfo { Name = "Dave", ImageUri = new Uri("https://www.tron-society.com/img/team/dave.jpg") },
+                    Type = ChatType.Normal,
+                    LastMessage = new ChatMessage { Message = "See you on monday!", Timestamp = DateTime.Now.AddHours(-2) },
+                    UnreadMessagesCount = 0
+                });
+                ChatEntries.Add(new ChatEntry
+                {
+                    GroupInfo = new GroupInfo { Name = "Tron Society", ImageUri = new Uri("https://www.tron-society.com/img/team/logo_en.png") },
+                    Type = ChatType.Group,
+                    LastMessage = new ChatMessage { Sender = new UserInfo { Name = "Padal" }, Message = "Any progress guys?", Timestamp = DateTime.Now.AddHours(-6) },
+                    UnreadMessagesCount = 3
+                });
+                ChatEntries.Add(new ChatEntry
+                {
+                    ChatPartner = new UserInfo { Name = "Luca", ImageUri = new Uri("https://www.tron-society.com/img/team/luca.jpg") },
+                    Type = ChatType.Normal,
+                    LastMessage = new ChatMessage { Message = "Bye Bye", Timestamp = DateTime.Now.AddDays(-1) },
+                    UnreadMessagesCount = 0
+                });
+                ChatEntries.Add(new ChatEntry
+                {
+                    ChatPartner = new UserInfo { Name = "Marius", ImageUri = new Uri("https://www.tron-society.com/img/team/marius.jpg") },
+                    Type = ChatType.Normal,
+                    LastMessage = new ChatMessage { Message = "Lol what the hell are you doin?", Timestamp = DateTime.Now.AddDays(-3) },
+                    UnreadMessagesCount = 43
+                });
+                ChatEntries.Add(new ChatEntry
+                {
+                    ChatPartner = new UserInfo { Name = "Simon", ImageUri = new Uri("https://www.tron-society.com/img/team/simon.jpg") },
+                    Type = ChatType.Normal,
+                    LastMessage = new ChatMessage { Message = "When do you have time?", Timestamp = DateTime.Now.AddDays(-35) },
+                    UnreadMessagesCount = 2
+                });
+            }
         }
     }
 }
