94b5fc5ddabb1fcaabc859ed2e71aca34bc8f305
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-11-29 01:55:52 +0100

    Added base for Chat Entry cells

---
+++ src/Converse/Views/Chat/GroupChatViewCell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Converse.Views.Chat
+{
+    public partial class GroupChatViewCell : ViewCell
+    {
+        public static readonly BindableProperty FooProperty =
+            BindableProperty.Create(nameof(Foo), typeof(string), typeof(GroupChatViewCell), null);
+
+        public GroupChatViewCell()
+        {
+            InitializeComponent();
+        }
+
+        public string Foo
+        {
+            get => (string)GetValue(FooProperty);
+            set => SetValue(FooProperty, value);
+        }
+    }
+}
---
+++ src/Converse/Views/Chat/NormalChatViewCell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Converse.Views.Chat
+{
+    public partial class NormalChatViewCell : ViewCell
+    {
+        public static readonly BindableProperty FooProperty =
+            BindableProperty.Create(nameof(Foo), typeof(string), typeof(NormalChatViewCell), null);
+
+        public NormalChatViewCell()
+        {
+            InitializeComponent();
+        }
+
+        public string Foo
+        {
+            get => (string)GetValue(FooProperty);
+            set => SetValue(FooProperty, value);
+        }
+    }
+}
