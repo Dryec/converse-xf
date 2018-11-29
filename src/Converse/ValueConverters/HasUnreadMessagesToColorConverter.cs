db2c8f1738694886b839c220682c2616a669e4be
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-11-29 02:02:05 +0100

    Added unread messages color and value converter

---
+++ src/Converse/ValueConverters/HasUnreadMessagesToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Converse.ValueConverters
+{
+    public class HasUnreadMessagesToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool hasUnreadMessages)
+            {
+                return hasUnreadMessages ? (Color)App.Current.Resources["UnreadMessagesColor"] : Color.Gray;
+            }
+            return Color.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
