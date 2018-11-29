f4132d05e3b7b062418357223e45f66ab45f6d83
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-11-29 18:41:58 +0100

    added colon and datetime value converters

---
+++ src/Converse/ValueConverters/AddColonToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Converse.ValueConverters
+{
+    public class AddColonToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string text)
+            {
+                return text + ":";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Converse/ValueConverters/DateTimeToEasyReadableStringConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Converse.ValueConverters
+{
+    public class DateTimeToEasyReadableStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime time)
+            {
+                var now = DateTime.Now;
+                if(time.Year == now.Year && time.Month == now.Month && time.Day == now.Day)
+                {
+                    return time.ToShortTimeString();
+                }
+                else if(time.Year == now.Year && time.Month == now.Month && time.Day == now.Day-1)
+                {
+                    return "Yesterday";
+                }
+                else if(time.Year == now.Year && time.Month == now.Month && time.Day < now.Day)
+                {
+                    return time.ToString("dddd");
+                }
+                else
+                {
+                    return time.ToShortDateString();
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
