b13cf6257e5b9728f24013884c366955543ff2f6
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-12-03 12:47:15 +0100

    added chat title bar view

---
+++ src/Converse/ChatTitleBarView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Converse.Views
+{
+    public partial class ChatTitleBarView : ContentView
+    {
+        public static readonly BindableProperty CanGoBackProperty =
+            BindableProperty.Create(nameof(CanGoBack), typeof(bool), typeof(ChatTitleBarView), null);
+
+        public ChatTitleBarView()
+        {
+            InitializeComponent();
+        }
+
+        public bool CanGoBack
+        {
+            get => (bool)GetValue(CanGoBackProperty);
+            set => SetValue(CanGoBackProperty, value);
+        }
+    }
+}
