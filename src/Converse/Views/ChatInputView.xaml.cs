cd4fc965da4da0125c5cde66d3f9d4f5f721506d
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-12-03 12:48:34 +0100

    added chat input view

---
+++ src/Converse/Views/ChatInputView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Converse.Views
+{
+    public partial class ChatInputView : ContentView
+    {
+        public static readonly BindableProperty FooProperty =
+            BindableProperty.Create(nameof(Foo), typeof(string), typeof(ChatInputView), null);
+
+        public ChatInputView()
+        {
+            InitializeComponent();
+        }
+
+        public string Foo
+        {
+            get => (string)GetValue(FooProperty);
+            set => SetValue(FooProperty, value);
+        }
+    }
+}
