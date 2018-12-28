01e51294b37419533708f77927851077436a12d9
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-12-28 23:40:16 +0100

    added sender name in group chats

--- src/Converse/DataTemplateSelectors/ChatMessageTemplateSelector.cs
+++ src/Converse/DataTemplateSelectors/ChatMessageTemplateSelector.cs
@@ -11,10 +11,16 @@ namespace Converse.DataTemplateSelectors
         readonly DataTemplate _rightMessageDataTemplate;
         readonly DataTemplate _leftMessageDataTemplate;
 
-        public ChatMessageTemplateSelector()
+        public ChatMessageTemplateSelector() : this(false)
+        {
+
+        }
+
+        public ChatMessageTemplateSelector(bool showName)
         {
             _rightMessageDataTemplate = new DataTemplate(typeof(RightChatMessageViewCell));
             _leftMessageDataTemplate = new DataTemplate(typeof(LeftChatMessageViewCell));
+            _leftMessageDataTemplate.SetValue(LeftChatMessageViewCell.ShowNameProperty, showName);
         }
 
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
--- src/Converse/Views/Chat/LeftChatMessageViewCell.xaml.cs
+++ src/Converse/Views/Chat/LeftChatMessageViewCell.xaml.cs
@@ -4,9 +4,19 @@ namespace Converse.Views.Chat
 {
     public partial class LeftChatMessageViewCell
     {
+        public static readonly BindableProperty ShowNameProperty =
+            BindableProperty.Create(nameof(ShowName), typeof(bool), typeof(LeftChatMessageViewCell), true);
+
         public LeftChatMessageViewCell()
         {
             InitializeComponent();
         }
+
+        public bool ShowName
+        {
+            get => (bool)GetValue(ShowNameProperty);
+            set => SetValue(ShowNameProperty, value);
+        }
+
     }
 }
